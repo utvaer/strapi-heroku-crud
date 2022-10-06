d4e6d164fe7084542225a1f7db5de08465d44072
Tiffany Utvaer Gasser <tiffany.u.gasser@gmail.com>
2022-10-06 12:06:11 +0200

    database.js updates

--- config/database.js
+++ config/database.js
@@ -1,28 +1,53 @@
-// const path = require("path");
-
 const { parse } = require("pg-connection-string");
 
 module.exports = ({ env }) => {
   const { host, port, database, user, password } = parse(env("DATABASE_URL"));
 
   return {
-    connection: {
-      client: "postgres",
-      connection: {
-        host,
-        port,
-        database,
-        user,
-        password,
-        schema: env("DATABASE_SCHEMA", "public"), // Not required
-        ssl: {
-          rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+    defaultConnection: "default",
+    connections: {
+      default: {
+        connector: "bookshelf",
+        settings: {
+          client: "postgres",
+          host,
+          port,
+          database,
+          username: user,
+          password,
+          ssl: { rejectUnauthorized: false },
         },
+        options: {},
       },
-      debug: false,
     },
   };
 };
+
+// // const path = require("path");
+
+// const { parse } = require("pg-connection-string");
+
+// module.exports = ({ env }) => {
+//   const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
+//   return {
+//     connection: {
+//       client: "postgres",
+//       connection: {
+//         host,
+//         port,
+//         database,
+//         user,
+//         password,
+//         schema: env("DATABASE_SCHEMA", "public"), // Not required
+//         ssl: {
+//           rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+//         },
+//       },
+//       debug: false,
+//     },
+//   };
+// };
 
 // Use this configuration for an SQLite database on your machine.
 // module.exports = ({ env }) => ({
